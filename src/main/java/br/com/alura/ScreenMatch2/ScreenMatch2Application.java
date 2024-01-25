9de5eddccf4ec4095fd33d2ffb034f2398812a6e
Bruno Sigrist[ <brunom.sigrist@hotmail.com>
2024-01-25 13:59:01 -0300

    Visualizando todas as temporadas e seus episódios de uma determinada série

--- src/main/java/br/com/alura/ScreenMatch2/ScreenMatch2Application.java
+++ src/main/java/br/com/alura/ScreenMatch2/ScreenMatch2Application.java
@@ -2,11 +2,15 @@ package br.com.alura.ScreenMatch2;
 
 import br.com.alura.ScreenMatch2.model.DadosEpisodio;
 import br.com.alura.ScreenMatch2.model.DadosSerie;
+import br.com.alura.ScreenMatch2.model.DadosTemporada;
 import br.com.alura.ScreenMatch2.service.ConsumoAPI;
 import br.com.alura.ScreenMatch2.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenMatch2Application  implements CommandLineRunner { //Criar algumas chamadas (CommandLineRunner)
@@ -28,5 +32,15 @@ public class ScreenMatch2Application  implements CommandLineRunner { //Criar alg
 		json = consumoAPI.obterDados("https://www.omdbapi.com/?t=Reacher&season=1&episode=2&apikey=66418c3a");
 		DadosEpisodio dadosEpisodio = conversor.obterDado(json, DadosEpisodio.class);
 		System.out.println(dadosEpisodio);
+
+		List<DadosTemporada> temporadas = new ArrayList<>();
+
+		//listar as temporadas
+		for(int i = 1; i <= dados.totalTemporadas(); i++){
+			json = consumoAPI.obterDados("https://www.omdbapi.com/?t=Reacher&season=" + i + "&apikey=66418c3a");
+			DadosTemporada dadosTemporada = conversor.obterDado(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+		temporadas.forEach(System.out::println); //printar a lista apartir do forEach
 	}
 }
